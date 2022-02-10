e18707284e476526770d5a0fd125a1ef892a4842
benjaminrall <benr2508@aol.com>
2022-02-10 14:37:50 +0000

    finished buttons, started text boxes

--- canvas.py
+++ canvas.py
@@ -9,10 +9,13 @@ class Canvas:
         self.elements =[]
         self.width = width
         self.height = height
+        self.visible = True
         self.surface = pygame.Surface((width, height), pygame.SRCALPHA)
 
     def update(self, cam):
         self.surface.fill((0, 0, 0, 0))
+        if not self.visible:
+            return
         for element in self.elements:
             element.draw(self.surface)
         cam.blit(self.surface, cam.get_world_coord((0, 0)))
@@ -43,9 +46,12 @@ class Fill:
         self.label = label
         self.colour = colour
         self.opacity = opacity
+        self.visible = True
         self.fade = [False, 0, 0, 0, 0]  # (fading, targetOpacity, increment, incrementTime, previousIncrement)
 
     def draw(self, surface):
+        if not self.visible:
+            return
         if self.fade[0]:
             timeChange = time.time() - self.fade[4]
             if timeChange >= self.fade[3]:
@@ -57,7 +63,18 @@ class Fill:
                     self.fade[4] += timeChange - self.fade[3]
         surface.fill((self.colour[0], self.colour[1], self.colour[2], self.opacity * 255))
 
+    def set_visible(self, state):
+        if self.fade[0]:
+            self.opacity = self.fade[1]
+            self.fade[0] = False
+        self.visible = state
+
+    def toggle_visible(self):
+        self.set_visible(not self.visible)
+
     def fade_to(self, opacity, duration, update=0.01):
+        if not self.visible:
+            return
         if opacity < 0 or opacity > 1:
             raise Exception("Invalid opacity given")
         if self.fade[0]:
@@ -72,12 +89,13 @@ class Text:
 
     pygame.font.init()
 
-    def __init__(self, label, x, y, font, size, text="", colour=(0,0,0), antialiasing=True):
+    def __init__(self, label, pos, font, size, text="", colour=(0,0,0), antialiasing=True):
         self.label = label
-        self.x = x
-        self.y = y
+        self.x = pos[0]
+        self.y = pos[1]
         self.font = pygame.font.SysFont(font, size)
         self.colour = colour
+        self.visible = True
         self.render(text, colour, antialiasing)
 
     def render(self, text, colour=None, antialiasing=True):
@@ -88,34 +106,47 @@ class Text:
         self.text = self.font.render(text, antialiasing, colour)
 
     def draw(self, surface):
+        if not self.visible:
+            return
         surface.blit(self.text, (self.x, self.y))
+
+    def set_visible(self, state):
+        self.visible = state
+
+    def toggle_visible(self):
+        self.set_visible(not self.visible)
 
 # Python Button object
 # A UI element that creates an interactable button
 # Dependencies : pygame
 class Button:
-    def __init__(self, label, x, y, width=None, height=None, text=None, colour=None, hoverColour=None, clickColour=None, animation=None, function=None):
+    def __init__(self, label, pos, dimensions=None, text=None, colour=None, hoverColour=None, clickColour=None, animation=None, onClick=None):
         if animation is not None and "default" not in animation:
             animation = None
-        if animation is None and (width is None or height is None or text is None or colour is None):
+        if animation is None and (dimensions is None or text is None or colour is None):
             raise Exception("Animation or all other parameters must be specified.")
         self.label = label
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height= height
+        self.x = pos[0]
+        self.y = pos[1]
+        self.animation = animation
+        self.animated = animation is not None
+        self.image = animation["default"] if self.animated else None
+        self.width = self.image.get_width() if self.animated else dimensions[0]
+        self.height= self.image.get_height() if self.animated else dimensions[1]
         self.text = text
         self.drawingColour = colour
         self.colour = colour
         self.hoverColour = hoverColour
         self.clickColour = clickColour
-        self.animation = animation
-        self.animated = animation is not None
-        self.function = function
+        self.visible = True
+        self.enabled = True
+        self.onClick = onClick
 
     def draw(self, surface):
+        if not self.visible:
+            return
         if self.animated:
-            pass
+            surface.blit(self.image, (self.x, self.y))
         else:
             pygame.draw.rect(surface, self.drawingColour, (self.x, self.y, self.width, self.height))
             surface.blit(self.text.text, (
@@ -123,13 +154,32 @@ class Button:
                 self.y + (self.height / 2) - (self.text.text.get_height() / 2) + self.text.y
             ))
 
+    def set_visible(self, state):
+        if self.animated:
+            self.image = self.animation["default"]
+        else:
+            self.drawingColour = self.colour
+        self.visible = state
+
+    def toggle_visible(self):
+        self.set_visible(not self.visible)
+
+    def set_enabled(self, state):
+        self.enabled = state
+
+    def toggle_enabled(self):
+        self.set_enabled(not self.enabled)
+
     def hover(self, pos):
-        if (self.animation is not None and "hover" not in self.animation) or self.hoverColour is None:
+        if not self.visible or (self.animated and "hover" not in self.animation) or (self.hoverColour is None and not self.animated):
             return
         relX = pos[0] - self.x
         relY = pos[1] - self.y
         if self.animated:
-            pass
+            if 0 <= relX <= self.width and 0 <= relY <= self.height:
+                self.image = self.animation["hover"]
+            else:
+                self.image = self.animation["default"]
         else:
             if 0 <= relX <= self.width and 0 <= relY <= self.height:
                 self.drawingColour = self.hoverColour
@@ -137,17 +187,95 @@ class Button:
                 self.drawingColour = self.colour
 
     def click(self, pos):
+        if not self.visible or not self.enabled:
+            return
         relX = pos[0] - self.x
         relY = pos[1] - self.y
         if self.animated:
             if 0 <= relX <= self.width and 0 <= relY <= self.height:
-                if self.function is not None:
-                    self.function()
+                if self.onClick is not None:
+                    self.onClick()
+                if "click" in self.animation:
+                    self.image = self.animation["click"]
+            else:
+                self.image = self.animation["default"]
         else:
             if 0 <= relX <= self.width and 0 <= relY <= self.height:
-                if self.function is not None:
-                    self.function()
+                if self.onClick is not None:
+                    self.onClick()
                 if self.clickColour is not None:
                     self.drawingColour = self.clickColour
             else:
-                self.drawingColour = self.colour  
+                self.drawingColour = self.colour
+
+# Python Button object
+# A UI element that creates an interactable text box
+# Dependencies : pygame
+class TextBox:
+    def __init__(self, label, pos, dimensions, text, colour, borderColour=None, borderWidth=1, hoverColour=None, activeColour=None, onEnter=None):
+        self.label = label
+        self.x = pos[0]
+        self.y = pos[1]
+        self.width = dimensions[0]
+        self.height = dimensions[1]
+        self.text = text
+        self.drawingColour = colour
+        self.colour = colour
+        self.hoverColour = hoverColour
+        self.activeColour = activeColour
+        self.border = borderColour is not None and borderWidth > 0
+        self.borderColour = borderColour
+        self.borderWidth = borderWidth if self.border else 0
+        self.active = False
+        self.visible = True
+        self.enabled = True
+        self.onEnter = onEnter
+    
+    def draw(self, surface):
+        if self.border:
+            pygame.draw.rect(surface, self.borderColour, (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(surface, self.drawingColour, (
+            self.x + self.borderWidth, self.y + self.borderWidth, 
+            self.width - (2 * self.borderWidth), self.height - (2 * self.borderWidth)
+        ))
+        surface.blit(self.text.text, (
+            self.x + self.borderWidth + (self.text.text.get_height() / 4), self.y + (self.height / 2) - (self.text.text.get_height() / 2)
+        ))
+        
+    def set_visible(self, state):
+        self.visible = state
+
+    def toggle_visible(self):
+        self.set_visible(not self.visible)
+
+    def set_enabled(self, state):
+        self.enabled = state
+
+    def toggle_enabled(self):
+        self.set_enabled(not self.enabled)
+
+    def hover(self, pos):
+        if not self.visible or self.hoverColour is None or self.active:
+            return
+        relX = pos[0] - self.x
+        relY = pos[1] - self.y
+        if 0 <= relX <= self.width and 0 <= relY <= self.height:
+            self.drawingColour = self.hoverColour
+        else:
+            self.drawingColour = self.colour
+
+    def click(self, pos):
+        if not self.visible or not self.enabled:
+            return
+        relX = pos[0] - self.x
+        relY = pos[1] - self.y
+        if 0 <= relX <= self.width and 0 <= relY <= self.height:
+            self.active = True
+            if self.activeColour is not None:
+                self.drawingColour = self.activeColour
+        else:
+            self.active = False
+            self.drawingColour = self.colour
+
+    def input_event(self):
+        pass
--- testing.py
+++ testing.py
@@ -1,6 +1,7 @@
 import pygame
 from personallib.camera import Camera
 from personallib.canvas import *
+import os
 
 # Constants
 WIN_WIDTH = 800
@@ -9,7 +10,7 @@ FRAMERATE = 60
 
 # Pygame Setup
 win = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-pygame.display.set_caption("NAME")
+pygame.display.set_caption("Canvas Testing")
 clock = pygame.time.Clock()
 
 # Methods
@@ -28,15 +29,24 @@ def button5():
 cam = Camera(win, 0, 0, 1)
 ui = Canvas(WIN_WIDTH, WIN_HEIGHT)
 ui.add_element(Fill("fill", (0, 0, 0), 0))
-ui.add_element(Text("text1", 20, 20, "georgia", 48, "test"))
-ui.add_element(Button("button1", 200, 100, 120, 60, Text("buttonText1", 0, 0, "georgia", 20, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), function=button1))
-ui.add_element(Button("button2", 200, 200, 120, 60, Text("buttonText2", 0, 0, "georgia", 20, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), function=button2))
-ui.add_element(Button("button3", 200, 300, 120, 60, Text("buttonText3", 0, 0, "georgia", 20, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), function=button3))
-ui.add_element(Button("button4", 200, 400, 120, 60, Text("buttonText4", 0, 0, "georgia", 20, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), function=button4))
-ui.add_element(Button("button5", 200, 500, 120, 60, Text("buttonText5", 0, 0, "georgia", 20, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), function=button5))
+ui.add_element(Text("text1", (20, 20), "georgia", 48, "test"))
+
+button_animation = {
+    "default": pygame.image.load(os.path.join("test_imgs", "default.png")),
+    "hover": pygame.image.load(os.path.join("test_imgs", "hover.png")),
+    "click": pygame.image.load(os.path.join("test_imgs", "click.png"))
+}
+
+ui.add_element(Button("button1", (185, 80), animation=button_animation, onClick=button1))
+ui.add_element(Button("button2", (200, 200), (120, 60), Text("buttonText2", (0, 0), "georgia", 24, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), onClick=button2))
+ui.add_element(Button("button3", (200, 300), (120, 60), Text("buttonText3", (0, 0), "georgia", 24, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), onClick=button3))
+ui.add_element(Button("button4", (200, 400), (120, 60), Text("buttonText4", (0, 0), "georgia", 24, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), onClick=button4))
+ui.add_element(Button("button5", (200, 500), (120, 60), Text("buttonText5", (0, 0), "georgia", 24, "click me!"), (200, 200, 200), (150, 150, 150), (100, 100, 100), onClick=button5))
+ui.add_element(TextBox("textBox1", (400, 100), (160, 30), Text("textBoxText1", (0, 0), "georgia", 16, "enter..."), (250, 250, 250), (0, 0, 0), 1, (230, 230, 230), (200, 200, 200)))
 
 # Variables
 running = True
+msg = ""
 
 # Main Loop
 if __name__ == '__main__':
@@ -46,11 +56,20 @@ if __name__ == '__main__':
                 running = False
                 pygame.quit()
                 exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_BACKSPACE:
+                    msg = msg[:-1]
+                else:
+                    msg += event.unicode    # exclude escape, tab, delete, enter
+                ui.find_element("text1").render(msg)
+                ui.run_method_on_type(TextBox, "input_event", [])
             elif event.type == pygame.MOUSEMOTION:
                 ui.run_method_on_type(Button, "hover", [pygame.mouse.get_pos()])
+                ui.run_method_on_type(TextBox, "hover", [pygame.mouse.get_pos()])
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     ui.run_method_on_type(Button, "click", [pygame.mouse.get_pos()])
+                    ui.run_method_on_type(TextBox, "click", [pygame.mouse.get_pos()])
             elif event.type == pygame.MOUSEBUTTONUP:
                 if event.button == 1:
                     ui.run_method_on_type(Button, "hover", [pygame.mouse.get_pos()])
